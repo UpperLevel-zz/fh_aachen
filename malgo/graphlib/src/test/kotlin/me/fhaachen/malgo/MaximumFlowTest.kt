495a004cdec9065ae66442afe1de4c32f049b69c
rist <steffen.richter@soptim.de>
2022-06-07 19:06:11 +0200

    Review MaxFlow

--- malgo/graphlib/src/test/kotlin/me/fhaachen/malgo/MaximumFlowTest.kt
+++ malgo/graphlib/src/test/kotlin/me/fhaachen/malgo/MaximumFlowTest.kt
@@ -1,30 +1,30 @@
 package me.fhaachen.malgo
 
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class MaximumFlowTest {
 
-    @Test
-    internal fun edmondsKarp_G_1_2() {
-        val resourceName = "G_1_2.txt"
+    @ParameterizedTest
+    @MethodSource("testParameters")
+    internal fun edmondsKarp(resourceName: String, result: Double) {
         val graph = GraphTest.toDiGraph(resourceName)
         val maximumFlow = MaxFlow.edmondsKarp(graph, 0, 7)
+        assertThat(maximumFlow).isEqualTo(result)
         println(maximumFlow)
     }
 
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            "Fluss.txt",
-            "Fluss2.txt"
-        ]
-    )
-    internal fun edmondsKarp(resourceName: String) {
-        val graph = GraphTest.toDiGraph(resourceName)
-        val maximumFlow = MaxFlow.edmondsKarp(graph, 0, 7)
-        println(maximumFlow)
+    private fun testParameters(): Stream<Arguments?>? {
+        return Stream.of(
+            Arguments.of("Fluss.txt", 4.0),
+            Arguments.of("Fluss2.txt", 5.0),
+            Arguments.of("G_1_2.txt", 0.75447)
+        )
     }
 
 }
